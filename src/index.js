6c02bea04b58eadb476a31be69cf69ba5d09c1ca
Arkadiusz Milewski <asmilewski@gmail.com>
2020-11-08 10:36:04 +0100

    componentDidMount() added and restructered

--- src/index.js
+++ src/index.js
@@ -6,19 +6,24 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = { lat: null,  errorMessage: '' };
+  }
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we called setState!!!!
-        this.setState({ lat: position.coords.latitude })
-      },
-      (err) => console.log(err)
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message})
     );
   }
-  
   render() {
-
-    return <div>Latitude: {this.state.lat} </div>
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+      }
+    if (!this.state.errorMessage && this.state.lat) {
+    return <div>Latitude: { this.state.lat } </div>
+      }
+    if (!this.state.errorMessage && !this.state.lat) {
+      return <div>Loading...</div>
+      }
   }
 }
 
